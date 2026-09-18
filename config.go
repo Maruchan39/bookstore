9c5ffc91d5afcb645ba9418114dbe7bf7d2c2b2d
Maruchan39 <martynas.baltutis@gmail.com>
2026-09-18 15:58:49 +0300

    gorm and mysql setup in main

--- config.go
+++ config.go
@@ -1,5 +1,8 @@
 package main
 
+import "gorm.io/gorm"
+
 type Config struct {
 	port string
+	db   *gorm.DB
 }
--- main.go
+++ main.go
@@ -4,9 +4,23 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
+
+type Book struct {
+	ID          string `gorm:"type:varchar(36);primaryKey"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	Title       string  `gorm:"type:varchar(255);not null"`
+	Author      string  `gorm:"type:varchar(255);not null"`
+	Price       float64 `gorm:"type:decimal(10,2);not null;default:0"`
+	IsAvailable bool    `gorm:"not null;default:true"`
+}
 
 func main() {
 	godotenv.Load(".env")
@@ -16,8 +30,23 @@ func main() {
 		log.Fatal("PORT environment variable is not set")
 	}
 
+	dbUrl := os.Getenv("DB_URL")
+	if dbUrl == "" {
+		log.Fatal("DB_URL environment variable is not set")
+	}
+
+	db, err := gorm.Open(mysql.Open(dbUrl), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database")
+	}
+
+	if err := db.AutoMigrate(&Book{}); err != nil {
+		log.Fatal("failed to migrate database")
+	}
+
 	cfg := Config{
 		port: port,
+		db:   db,
 	}
 
 	mux := http.NewServeMux()
